7d8196a2e0c88793d80a4b161c9e123662a521fa
k4rolin4 <karolina.kozinska16@gmail.com>
2022-05-07 02:10:35 +0200

    cancel

--- projects/tasks/src/lib/adapters/primary/ui/add-task.component.ts
+++ projects/tasks/src/lib/adapters/primary/ui/add-task.component.ts
@@ -1,20 +1,30 @@
 import { FormGroup, FormControl } from '@angular/forms';
 import { Component, ViewEncapsulation, ChangeDetectionStrategy, Inject } from '@angular/core';
 import { ADDS_TASK_DTO, AddsTaskDtoPort } from '../../../application/ports/secondary/adds-task.dto-port';
+import { REMOVES_TASK_DTO, RemovesTaskDtoPort } from '../../../application/ports/secondary/removes-task.dto-port';
 
-@Component({ selector: 'lib-add-task', templateUrl: './add-task.component.html', encapsulation: ViewEncapsulation.None, changeDetection: ChangeDetectionStrategy.OnPush })
+@Component({ 
+  selector: 'lib-add-task', 
+  templateUrl: './add-task.component.html', 
+  encapsulation: ViewEncapsulation.None, 
+  changeDetection: ChangeDetectionStrategy.OnPush 
+})
 export class AddTaskComponent {
   readonly addTask: FormGroup = new FormGroup({
     task: new FormControl()
   });
 
-  constructor(@Inject(ADDS_TASK_DTO) private _addsTaskDto: AddsTaskDtoPort) {
+  constructor(
+    @Inject(ADDS_TASK_DTO) private _addsTaskDto: AddsTaskDtoPort) {
   }
 
   onAddTaskSubmited(addTask: FormGroup): void {
     this._addsTaskDto.add({
       task: addTask.get('task')?.value
     });
+    this.addTask.reset();
+  }
+  cancelAnimationFrame(): void{
     this.addTask.reset();
   }
 }
